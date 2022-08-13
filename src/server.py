ac94e6216d0d4a45b3ef8f10648c1122f967adbb
boyufan <fbygg@126.com>
2022-08-13 09:51:34 +0300

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 import copy
+from dataclasses import replace
 import logging
 import gc
 
@@ -31,6 +32,7 @@ class Server(object):
 
         self.seed = global_config['seed']
         self.device = global_config['device']
+        self.mp_flag = global_config['is_mp']
         
         self.data_path = data_config['data_path']
         self.dataset_name = data_config['dataset_name']
@@ -89,9 +91,9 @@ class Server(object):
         print(message); logging.info(message)
         del message; gc.collect()
     
-    def transmit_model(self, sample_client_indices=None):
+    def transmit_model(self, sampled_client_indices=None):
         """Send the updated global model to selected clients"""
-        if sample_client_indices is None:
+        if sampled_client_indices is None:
             assert (self._round == 0) or (self._round == self.num_rounds)
 
             for client in tqdm(self.clients, leave=False):
@@ -103,12 +105,166 @@ class Server(object):
         else:
             assert self._round != 0
 
-            for idx in tqdm(sample_client_indices, leave=False):
+            for idx in tqdm(sampled_client_indices, leave=False):
                 self.clients[idx].model = copy.deepcopy(self.model)
             
             message = f"[Round: {str(self._round).zfill(4)}] ...successfully transmitted models to {str(len(sampled_client_indices))} selected clients!"
             print(message); logging.info(message)
             del message; gc.collect()
+    
+    def sample_clients(self):
+        """Sample clients from clients pool"""
+        message = f'[Round: {str(self._round).zfill(4)}] Select clients...!'
+        print(message); logging.info(message)
+        del message; gc.collect()
+
+        num_sampled_clients = max(int(self.fraction * self.num_clients), 1)
+        # 随机采样靠下面这条语句实现，choice函数实现从一个list中随机取出size大小的值，然后排序，完成随机采样
+        sampled_client_indices = sorted(np.random.choice(a=[i for i in range(self.num_clients)], size=num_sampled_clients, replace=False).tolist())
+        return sampled_client_indices
+    
+    def update_selected_clients(self, sampled_client_indices):
+        """Call 'client_update' function of each selected client"""
+        message = f"[Round: {str(self._round).zfill(4)}] Start updating selected {len(sampled_client_indices)} clients...!"
+        print(message); logging.info(message)
+        del message; gc.collect()
+
+        selected_total_size = 0
+        for idx in tqdm(sampled_client_indices, leave=False):
+            self.clients[idx].client_update()
+            selected_total_size += len(self.clients[idx])
+        
+        message = f"[Round: {str(self._round).zfill(4)}] ...{len(sampled_client_indices)} clients are selected and updated (with total sample size: {str(selected_total_size)})!"
+        print(message); logging.info(message)
+        del message; gc.collect()
+
+        return selected_total_size
+    
+    def average_model(self, sampled_client_indices, coefficients):
+        """Average the updated models"""
+        message = f"[Round: {str(self._round).zfill(4)}] Aggregate updated weights of {len(sampled_client_indices)} clients...!"
+        print(message); logging.info(message)
+        del message; gc.collect()
+
+        averaged_weights = OrderedDict()
+        for it, idx in tqdm(enumerate(sampled_client_indices), leave=False):
+            local_weights = self.clients[idx].model.state_dict()
+            for key in self.model.state_dict().keys():
+                if it == 0:
+                    averaged_weights[key] = coefficients[it] * local_weights[key]
+                else:
+                    averaged_weights[key] += coefficients[it] * local_weights[key]
+        self.model.load_state_dict(averaged_weights)
+
+        message = f"[Round: {str(self._round).zfill(4)}] ...updated weights of {len(sampled_client_indices)} clients are successfully averaged!"
+        print(message); logging.info(message)
+        del message; gc.collect()
+    
+    def evaluate_selected_models(self, sampled_client_indices):
+        """Call 'client_evaluate' function of each selected client"""
+        message = f"[Round: {str(self._round).zfill(4)}] Evaluate selected {str(len(sampled_client_indices))} clients' models...!"
+        print(message); logging.info(message)
+        del message; gc.collect()
+
+        for idx in sampled_client_indices:
+            self.clients[idx].client_evaluate()
+        
+        message = f"[Round: {str(self._round).zfill(4)}] ...finished evaluation of {str(len(sampled_client_indices))} selected clients!"
+        print(message); logging.info(message)
+        del message; gc.collect()
+
+    def mp_evaluate_selected_models(self, selected_index):
+        """Multiprocess-applied version"""
+        self.clients[selected_index].client_evaluate()
+        return True
+    
+    def train_federated_model(self):
+        """Do the federated training"""
+        # select fraction of clients randomly
+        sampled_client_indices = self.sample_clients()
+
+        # send global model to the selected clients
+        self.transmit_model(sampled_client_indices)
+
+        # updated selected clients with local dataset
+        if self.mp_flag:
+            with pool.ThreadPool(processes=cpu_count() - 1) as workhorse:
+                selected_total_size = workhorse.map(self.mp_update_selected_clients, sampled_client_indices)
+            selected_total_size = sum(selected_total_size)
+        else:
+            selected_total_size = self.update_selected_clients(sampled_client_indices)
+
+        if self.mp_flag:
+            message = f"[Round: {str(self._round).zfill(4)}] Evaluate selected {str(len(sampled_client_indices))} clients' models...!"
+            print(message); logging.info(message)
+            del message; gc.collect()
+
+            with pool.ThreadPool(processes=cpu_count() - 1) as workhorse:
+                workhorse.map(self.mp_evaluate_selected_models, sampled_client_indices)
+        else:
+            self.evaluate_selected_models(sampled_client_indices)
+        
+        mixing_coefficients = [len(self.clients[idx]) / selected_total_size for idx in sampled_client_indices]
+
+        # average each updated model parameters of the selected clients and update the global model
+        self.average_model(sampled_client_indices, mixing_coefficients)
+    
+    def evaluate_global_model(self):
+        self.model.eval()
+        self.model.to(self.device)
+
+        test_loss, correct = 0, 0
+        with torch.no_grad():
+            for data, labels in self.dataloader:
+                data, labels = data.float().to(self.device), labels.long().to(self.device)
+                outputs = self.model(data)
+                test_loss += eval(self.criterion)()(outputs, labels).item()
+                
+                predicted = outputs.argmax(dim=1, keepdim=True)
+                correct += predicted.eq(labels.view_as(predicted)).sum().item()
+        
+        test_loss = test_loss / len(self.dataloader)
+        test_accuracy = correct / len(self.data)
+        return test_loss, test_accuracy
+    
+    def fit(self):
+        """Execute the whole process of the feredated learning"""
+        self.results = {'loss': [], 'accuracy': []}
+        for r in range(self.num_rounds):
+            self._round = r + 1
+            self.train_federated_model()
+            test_loss, test_accuracy = self.evaluate_global_model()
+
+            self.results['loss'].append(test_loss)
+            self.results['accuracy'].append(test_accuracy)
+
+            self.writer.add_scalars(
+                'Loss',
+                {f'[{self.dataset_name}]_{self.model.name} C_{self.fraction}, E_{self.local_epochs}, B_{self.batch_size}, IID_{self.iid}':test_loss}, 
+                self._round
+                )
+            self.writer.add_scalars(
+                'Accuracy', 
+                {f"[{self.dataset_name}]_{self.model.name} C_{self.fraction}, E_{self.local_epochs}, B_{self.batch_size}, IID_{self.iid}": test_accuracy},
+                self._round
+                )
+            
+            message = f"[Round: {str(self._round).zfill(4)}] Evaluate global model's performance...!\
+                \n\t[Server] ...finished evaluation!\
+                \n\t=> Loss: {test_loss:.4f}\
+                \n\t=> Accuracy: {100. * test_accuracy:.2f}%\n"            
+            print(message); logging.info(message)
+            del message; gc.collect()
+        self.transmit_model()
+    
+
+        
+
+
+
+
+
+
     
     
 
